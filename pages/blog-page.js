933384cd1da344c64fbf6d41f3c9aca484c8175d
ryoma-kaneko <nekowafu0413@gmail.com>
2021-05-11 00:54:50 +0900

    initial commit

---
+++ pages/blog-page.js
@@ -0,0 +1,41 @@
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+
+import Link from "next/link";
+import { getAllPostData } from "../lib/posts";
+export default function BlogPage({ filteredPosts }) {
+  return (
+    <Layout title="Blog Page">
+      <ul>
+        {filteredPosts &&
+          filteredPosts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+      <Link href="/main-page">
+        <div className="flex cursor-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            />
+          </svg>
+          <span>Back to main page</span>
+        </div>
+      </Link>
+    </Layout>
+  );
+}
+export async function getStaticProps() {
+  const filteredPosts = await getAllPostData();
+  return {
+    props: { filteredPosts },
+    revalidate: 3
+  };
+}
